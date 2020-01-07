b7f4376fc477c8547a206a2cb03b0a3223bdb0c8
StudyCool <as.shevh@gmail.com>
2020-01-07 21:18:20 +0200

    fixed styles in banner

--- src/assets/js/nav-toggle.js
+++ src/assets/js/nav-toggle.js
@@ -2,5 +2,5 @@
 
 document.querySelector('.nav-toggle').onclick = function() {
   this.classList.toggle('close-nav');
-  document.getElementById('navigation').classList.toggle('open');
+  document.querySelector('navigation').classList.toggle('open');
 }
