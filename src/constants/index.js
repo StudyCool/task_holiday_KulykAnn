e26b2778272e801cc493d3f7842da9cf438b5096
StudyCool <as.shevh@gmail.com>
2020-01-10 23:28:28 +0200

    added dir constants

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const LINKS_ICON_MAP = new Map();
+LINKS_ICON_MAP.set( 'facebook.com', ['fab', 'fa-facebook'] );
+LINKS_ICON_MAP.set( 'twitter.com', ['fab', 'fa-twitter'] );
+LINKS_ICON_MAP.set( 'linkedin.com', ['fab', 'fa-linkedin'] );
+LINKS_ICON_MAP.set( 'plus.google.com', ['fab', 'fa-google-plus'] );
+LINKS_ICON_MAP.set( 'theglobe.com', ['fab', 'fa-globe'] );
+
+
+
+
