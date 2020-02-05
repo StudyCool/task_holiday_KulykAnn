0c998085d8b17bbc3185ac0852e68d12ebaaeb18
StudyCool <as.shevh@gmail.com>
2020-02-05 05:29:32 +0200

    added downloadSection

--- src/assets/js/nav-toggle.js
+++ src/assets/js/nav-toggle.js
@@ -4,25 +4,23 @@ document.querySelector('.nav-toggle').onclick = function () {
     this.classList.toggle('close-nav');
 //    console.log(this);
     document.getElementById('navigation').classList.toggle('open');
-}
-
-
-/*
-document.querySelector('nav-href-section').onclick= function() {
-  this.classList.toggle('active');
-  document.getElementById('navigation').classList.toggle('open');
 
 }
-*/
-function findActiveElement(e) {
-    const activeElem = e.target
+document.querySelector('.navigation').onclick = function () {
+    this.classList.toggle('close-nav');
+    document.getElementById('navigation').classList.toggle('open');
+}
+
+/*function findActiveElement(e) {
+    const activeElem = e.target;
 }
 
 const a_menu = document.getElementsByTagName('a').onclick = function () {
 
-};
-//console.log(a_menu);
+}
+
 
 
 const childElement = document.querySelector('nav-href-section');
-//console.log(childElement);
+
+ */
--- src/components/UsersList/components/UserCard/index.js
+++ src/components/UsersList/components/UserCard/index.js
@@ -1,38 +1,49 @@
 import './styles.scss';
 import createPicture from '../../../Picture';
+import link from '../../../ContactLink/index';
 
-export default function (user) {
-    const cardContainer = document.createElement('div');
-    const userPicture = createPicture(user.profilePicture, '/assets/images/user_icon.png', 'profile picture', ['userImg', 'img-responsive']);
-    cardContainer.appendChild(userPicture);
+export default function(user) {
+  const cardContainer = document.createElement('div');
+  const userPicture = createPicture(user.profilePicture,
+      '/assets/images/user_icon.png', 'profile picture',
+      ['userImg', 'img-responsive']);
+  cardContainer.appendChild(userPicture);
 
-    cardContainer.appendChild(createNameElem(user));
+  cardContainer.appendChild(createNameElem(user));
 
-    cardContainer.appendChild(createRoleElem(user));
+  cardContainer.appendChild(createRoleElem(user));
 
-    cardContainer.appendChild(createDescriptionElem(user));
+  cardContainer.appendChild(createDescriptionElem(user));
 
-    return cardContainer;
+  //cardContainer.appendChild(createSocialNetworksLinks(user));
+
+  return cardContainer;
 }
 
-
 function createNameElem(user) {
-    const employeeNameHeading = document.createElement('h4');
-    employeeNameHeading.classList.add('fullName');
-    employeeNameHeading.innerText = user.name;
-    return employeeNameHeading;
+  const employeeNameHeading = document.createElement('h4');
+  employeeNameHeading.classList.add('fullName');
+  employeeNameHeading.innerText = user.name;
+  return employeeNameHeading;
 }
 
 function createRoleElem(user) {
-    const employeeRoleHeading = document.createElement('h5');
-    employeeRoleHeading.classList.add('role');
-    employeeRoleHeading.innerText = user.role;
-    return employeeRoleHeading;
+  const employeeRoleHeading = document.createElement('h5');
+  employeeRoleHeading.classList.add('role');
+  employeeRoleHeading.innerText = user.role;
+  return employeeRoleHeading;
 }
 
 function createDescriptionElem(user) {
-    const employeeDescriptionParagraph = document.createElement('p');
-    employeeDescriptionParagraph.classList.add('description');
-    employeeDescriptionParagraph.innerText = user.description;
-    return employeeDescriptionParagraph;
+  const employeeDescriptionParagraph = document.createElement('p');
+  employeeDescriptionParagraph.classList.add('description');
+  employeeDescriptionParagraph.innerText = user.description;
+  return employeeDescriptionParagraph;
 }
+
+
+/*
+function createSocialNetworksLinks(user, link, arrayOfLinks = []) {
+arrayOfLinks = user.contacts;
+
+}*/
